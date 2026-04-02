ded39ab219d6286298661cc563b2e97da9d7aa4b
yohan frutiger <yohan.frutiger@gmail.com>
2026-04-02 15:36:10 +0200

    style: modification du style pour le composant Line.jsx

--- front/src/components/layout/Line.jsx
+++ front/src/components/layout/Line.jsx
@@ -1,7 +1,7 @@
 export default function () {
   return (
     <div>
-    <hr className="block my-8 border-t border-gray-500 w-14 mx-auto " />
+    <hr className="block my-8 border-champaign max-w-7xl mx-auto " />
     </div>
   )
 }
