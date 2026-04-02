21a842f9cdbaaf2bde904e77a4cbbb2e8d02c342
yohan frutiger <yohan.frutiger@gmail.com>
2026-04-02 14:44:31 +0200

    style: modification de la barre de navigation

--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -1,108 +1,81 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/images/logo.png";
 
 export default function Header() {
 
   const [isOpen, setIsOpen] = useState(false);
 
+  // Items de navigation
   const navItems = [
-    { to: "/", label: "Accueil" },
-    { to: "/about", label: "Qui sommes-nous ?" },
-    { to: "/categories", label: "Nos prestations" },
-    { to: "/prices", label: "Tarifs" },
-    { to: "/contact", label: "Contact & Devis" },
+    { to: "/", label: "Le gîte" },
+    { to: "/la-region", label: "La région" },
+    { to: "/venir", label: "Venir" }
   ];
 
-  return (
-    <>
-      <header className="h-16 bg-green/90 text-white shadow-lg fixed top-0 left-0 right-0 z-50">
-        <div className="h-full max-w-7xl mx-auto flex items-center justify-between">
-          {/* Logo + bande verte */}
-          <div className="flex items-center">
-            <div className="w-6 md:block" />
-              <NavLink to="/">
-                <img
-                  src={logo}
-                  alt="Canopées - Entreprise de paysagisme"
-                  className="bg-white h-16 px-5 w-auto max-w-[120px] lg:max-w-[200px] object-contain"
-                />
-              </NavLink>
-          </div>
+  return (<>
 
-          {/* Navigation desktop */}
-          <nav className="hidden md:flex items-center justify-center flex-1 md:gap-4 lg:gap-10 ">
-            {navItems.map((item) => (
-              <NavLink
-                key={item.to}
-                to={item.to}
-                className={({ isActive }) =>
-                  ` font-rosario text-xl font-medium transition-colors hover:text-orange ${isActive ? "text-orange" : "text-white"}`
-                }
-              >
-                {item.label}
-              </NavLink>
-            ))}
-          </nav>
+    <header className="h-16 mx-auto bg-champaign/90  rounded-b-sm  fixed top-0 left-0 right-0 z-50">
+      <div className="h-full flex items-center justify-between">
 
-
-          {/* BURGER BUTTON */}
-          <button
-            type="button"
-            onClick={() => setIsOpen(!isOpen)}
-            aria-expanded={isOpen}
-            aria-controls="mobile-menu"
-            aria-label="Ouvrir le menu de navigation"
-            className="md:hidden text-4xl p-2 m-2 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-orange-300 focus-visible:ring-offset-2 focus-visible:ring-offset-green-700 rounded"
-          >
-            {isOpen ? "" : "☰"}
-          </button>
+        {/* Logo*/}
+        <div className="ml-4 mt-1">
+          <NavLink to="/">
+            <img
+              src={logo}
+              alt="Logo gîte de séjour La Grange"
+              className=" h-16 px-2 sm:px-4 w-auto max-w-[120px] sm:max-w-[180px] object-contain"
+            />
+          </NavLink>
         </div>
-      </header>
 
-      {/* Burger menu */}
-      <div
-        className={`
+        {/* Navigation */}
+        <nav className="hidden sm:flex items-center justify-center flex-1 sm:gap-8 md:gap-10 lg:gap-36 mx-auto">
+          {navItems.map((item) => (
+            <NavLink
+              key={item.to}
+              to={item.to}
+              className={({ isActive }) =>
+                ` font-outfit text-green text-lg md:text-xl tracking-wide  ${isActive ? "font-black scale-110 underline underline-offset-8 cursor-default" : " transition-all duration-500 hover:scale-105 "}`
+              }
+            >
+              {item.label}
+            </NavLink>
+          ))}
+        </nav>
+
+        {/* Bouton Réserver */}
+        <div className="bg-orange rounded-md px-4 py-1 mr-8 md:mr-14 text-white text-base sm:text-lg   ">
+          <a href="">
+            Réserver
+          </a>
+        </div>
+
+        {/* Bouton burger */}
+        {/* <button
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-expanded={isOpen}
+          aria-controls="mobile-menu"
+          aria-label="Ouvrir le menu de navigation"
+          className="sm:hidden text-3xl p-2 m-2 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-orange-300 focus-visible:ring-offset-2 focus-visible:ring-offset-green-700 rounded"
+        >
+          {isOpen ? "" : "☰"}
+        </button> */}
+
+        {/* menu burger */}
+        {/* <div
+          className={`
           fixed inset-0 transition-opacity duration-300 z-40  
           ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}
         `}
-        onClick={() => setIsOpen(false)}
-      />
+          onClick={() => setIsOpen(false)}
+        /> */}
 
-      {/* Burger layer */}
-      <nav
-        className={`
-          fixed top-0 right-0 h-[350px] w-60 bg-gray-900 transform transition-transform duration-300 z-50 
-          ${isOpen ? "translate-x-0" : "translate-x-full"}
-        `}
-      >
-        {/* X button */}
-        <div className="flex justify-end mt-2 mr-4">
-          <button
-            onClick={() => setIsOpen(false)}
-            className="text-5xl text-white hover:text-orange "
-          >
-            ✕
-          </button>
-        </div>
+      </div>
+    </header>
 
-        {/* Menu */}
-        <ul className="flex flex-col items-start gap-5 mt-2 p-4 pr-4 bg-gray-900">
-          {navItems.map((item) => (
-            <li key={item.to}>
-              <NavLink
-                to={item.to}
-                onClick={() => setIsOpen(false)}
-                className={({ isActive }) =>
-                  `text-xl font-semibold font-rosario transition-colors ${isActive ? "text-orange" : "text-white"
-                  } hover:text-orange`}
-              >
-                {item.label}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </>
+   
+  </>
   );
 }
